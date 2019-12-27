425cd1abb74c80e9774c29f5179a66f10496f5b7
GarethBeer <gareth.beer1989@outlook.com>
2019-12-27 15:02:10 +0000

    Creating a real estate website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+/* Components */
+import NavBar from "./Components/NavBar";
+
+import "./Style/App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<NavBar />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../Style/nav-bar.css";
+
+function NavBar() {
+	return (
+		<div className="App">
+			<h1>Estates</h1>
+		</div>
+	);
+}
+
+export default NavBar;
